663228ac85fe91951bca3a2ba7ac5937bb9f1638
李奋鹏 <mmlp1170@htomail.com>
2021-03-24 20:03:55 +0800

    css_edit

--- config/config.js
+++ config/config.js
@@ -1,19 +1,19 @@
 const config = {
     port:'9999',
-    mysql:{
-        connectionLimit: 20,
-        host     : 'localhost',
-        user     : 'drip',
-        password : 'C8EWHCf7RTKak5tT',
-        database : 'drip'
-    },
     // mysql:{
     //     connectionLimit: 20,
     //     host     : 'localhost',
-    //     user     : 'root',
-    //     password : 'root',
+    //     user     : 'drip',
+    //     password : 'C8EWHCf7RTKak5tT',
     //     database : 'drip'
-    // }
+    // },
+    mysql:{
+        connectionLimit: 20,
+        host     : 'localhost',
+        user     : 'root',
+        password : 'root',
+        database : 'drip'
+    }
 }
 
 module.exports = config;
